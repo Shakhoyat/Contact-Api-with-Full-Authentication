2189348ba9ccb3263576051d06f4f24921c0d571
Shakhoyat <shakoyatsujon@gmail.com>
2025-06-23 03:18:34 +0600

    fix: streamline dotenv configuration and improve code comments for clarity

--- server.js
+++ server.js
@@ -5,31 +5,24 @@ import userRouter from "./Routes/UserRouter.js";
 import contactRouter from "./Routes/ContactRouter.js";
 import dotenv from "dotenv";
 
+dotenv.config({ path: ".env" }); // Only call this once
+
 const app = express();
 
-// Middleware to parse JSON bodies from incoming requests
 app.use(bodyParser.json());
 
-// User routes
 app.use("/api/user", userRouter);
-
-// Contact routes
 app.use("/api/contact", contactRouter);
 
-// Home route
 app.get("/", (req, res) => {
   res.send("Welcome to the Home Page!");
 });
-
-// Connect to MongoDB using Mongoose
-dotenv.config();
 
 mongoose
   .connect(process.env.MONGODB_URL, { dbName: process.env.DB_NAME })
   .then(() => console.log("MongoDB connection successful!"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
-// Start the Express server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
